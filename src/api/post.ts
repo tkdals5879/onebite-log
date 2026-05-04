3b3e2129226c24c7a8e6d714f8e5b00e8c437a33
sangminLee <smlee@takensoft.co.kr>
2026-05-04 17:56:49 +0900

    260504 7.4 24분10초부터 시작

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,10 +1,49 @@
 import { supabase } from "@/lib/supabase";
+import { uploadImage } from "./image";
+import type { PostEntity } from "@/types";
 
 export async function createPost(content: string) {
-  const { data, error } = await supabase.from("post").insert({
-    content,
-  });
+  const { data, error } = await supabase
+    .from("post")
+    .insert({
+      content,
+    })
+    .select()
+    .single();
+  // supabase 테이블에 아이템을 추가하고 반환값을 받기위해서는 select와single 메서드를 사용해줘야함
 
   if (error) throw error;
   return data;
 }
+
+export async function createPostWithImages({
+  content,
+  images,
+  userId,
+}: {
+  content: string;
+  images: File[];
+  userId: string;
+}) {
+  // 1. 포스트 생성
+  const post = await createPost(content);
+  if (images.length === 0) return post;
+
+  // 2. (스토리지에)이미지 업로드
+  const imageUrls = await Promise.all(
+    images.map((image) => {
+      const fileExtension = image.name.split(".").pop() || "webp";
+      const fileName = `${Date.now()}-${crypto.randomUUID()}.${fileExtension}`;
+      const filePath = `${userId}/${post.id}/${fileName}`;
+
+      return uploadImage({
+        file: image,
+        filePath,
+      });
+    }),
+  );
+
+  // 3. 포스트 테이블
+}
+
+export async function updatePost(post: Partial<PostEntity> & { id: number }) {}
--- src/components/modal/post-editor-modal.tsx
+++ src/components/modal/post-editor-modal.tsx
@@ -8,138 +8,137 @@ import { toast } from "sonner";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 
 type Image = {
-  file: File;
-  previewUrl: string;
+    file: File;
+    previewUrl: string;
 };
 
 export default function PostEditorModal() {
-  const { isOpen, close } = usePostEditorModal();
-  const [content, setContetnt] = useState("");
-  const [images, setImages] = useState<Image[]>([]);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+    const { isOpen, close } = usePostEditorModal();
+    const [content, setContent] = useState("");
+    const [images, setImages] = useState<Image[]>([]);
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
-    onSuccess: () => {
-      close();
-      toast.success("나의 이야기 등록 완료!", {
-        position: "top-center",
-      });
-    },
-    onError: (error) => {
-      toast.error("포스트 생성에 실패하셨습니다", {
-        position: "top-center",
-      });
-    },
-  });
+    const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
+        onSuccess: () => {
+            close();
+            toast.success("나의 이야기 등록 완료!", {
+                position: "top-center",
+            });
+        },
+        onError: (error) => {
+            toast.error("포스트 생성에 실패하셨습니다", {
+                position: "top-center",
+            });
+        },
+    });
 
-  const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      const files = Array.from(e.target.files);
+    const handleSelectImages = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            const files = Array.from(e.target.files);
 
-      files.forEach((file) => {
-        setImages((prev) => [
-          ...prev,
-          { file, previewUrl: URL.createObjectURL(file) },
-        ]);
-      });
-    }
+            files.forEach((file) => {
+                setImages((prev) => [
+                    ...prev,
+                    { file, previewUrl: URL.createObjectURL(file) },
+                ]);
+            });
+        }
 
-    e.target.value = "";
-  };
+        e.target.value = "";
+    };
 
-  const handleCloseModal = () => {
-    close();
-  };
+    const handleCloseModal = () => {
+        close();
+    };
 
-  const handleCreatePostClick = () => {
-    if (content.trim() === "") return;
-    createPost(content);
-  };
+    const handleCreatePostClick = () => {
+        if (content.trim() === "") return;
+        createPost(content);
+    };
 
-  const handleDeleteImage = (image: Image) => {
-    setImages((prevImages) =>
-      prevImages.filter((item) => item.previewUrl !== image.previewUrl),
+    const handleDeleteImage = (image: Image) => {
+        setImages((prevImages) =>
+            prevImages.filter((item) => item.previewUrl !== image.previewUrl),
+        );
+    };
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height =
+                textareaRef.current.scrollHeight + "px";
+        }
+    }, [content]);
+
+    useEffect(() => {
+        if (!isOpen) return;
+        textareaRef.current?.focus();
+        setContent("");
+        setImages([]);
+    }, [isOpen]);
+
+    return (
+        <Dialog open={isOpen} onOpenChange={handleCloseModal}>
+            <DialogContent className="max-h-[90vh]">
+                <DialogTitle>포스트 작성</DialogTitle>
+                <textarea
+                    disabled={isCreatePostPending}
+                    ref={textareaRef}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    className="max-h-125 min-h-25 focus:outline-none"
+                    placeholder="무슨 일이 있었나요?"
+                />
+                <input
+                    onChange={handleSelectImages}
+                    ref={fileInputRef}
+                    type="file"
+                    accept="image/*"
+                    multiple
+                    className="hidden"
+                />
+                {images.length > 0 && (
+                    <Carousel>
+                        <CarouselContent>
+                            {images.map((image) => (
+                                <CarouselItem className="basis-2/5" key={image.previewUrl}>
+                                    <div className="relative">
+                                        <img
+                                            src={image.previewUrl}
+                                            className="h-full w-full rounded-sm object-cover"
+                                        />
+                                        <div
+                                            onClick={() => handleDeleteImage(image)}
+                                            className="absolute top-0 right-0 m-1 cursor-pointer rounded-full bg-black/30 p-1"
+                                        >
+                                            <XIcon className="h-full w-full text-white" />
+                                        </div>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                    </Carousel>
+                )}
+                <Button
+                    onClick={() => {
+                        fileInputRef.current?.click();
+                    }}
+                    disabled={isCreatePostPending}
+                    variant={"outline"}
+                    className="cursor-pointer"
+                >
+                    <ImageIcon />
+                    이미지 추가
+                </Button>
+                <Button
+                    disabled={isCreatePostPending}
+                    onClick={handleCreatePostClick}
+                    className="cursor-pointer"
+                >
+                    저장
+                </Button>
+            </DialogContent>
+        </Dialog>
     );
-  };
-
-  useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.style.height = "auto";
-      textareaRef.current.style.height =
-        textareaRef.current.scrollHeight + "px";
-    }
-  }, [content]);
-
-  useEffect(() => {
-    if (!isOpen) return;
-    textareaRef.current?.focus();
-    setContetnt("");
-    // content내용이 있거나 images가 있다면 닫히기 전에 "작성중인 게시물이 삭제됩니다." 메시지 띄우고 네 누르면 닫히고 아니오 누르면 안닫히게 만들어보기
-    setImages([]);
-  }, [isOpen]);
-
-  return (
-    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
-      <DialogContent className="max-h-[90vh]">
-        <DialogTitle>포스트 작성</DialogTitle>
-        <textarea
-          disabled={isCreatePostPending}
-          ref={textareaRef}
-          value={content}
-          onChange={(e) => setContetnt(e.target.value)}
-          className="max-h-125 min-h-25 focus:outline-none"
-          placeholder="무슨 일이 있었나요?"
-        />
-        <input
-          onChange={handleSelectImages}
-          ref={fileInputRef}
-          type="file"
-          accept="image/*"
-          multiple
-          className="hidden"
-        />
-        {images.length > 0 && (
-          <Carousel>
-            <CarouselContent>
-              {images.map((image) => (
-                <CarouselItem className="basis-2/5" key={image.previewUrl}>
-                  <div className="relative">
-                    <img
-                      src={image.previewUrl}
-                      className="h-full w-full rounded-sm object-cover"
-                    />
-                    <div
-                      onClick={() => handleDeleteImage(image)}
-                      className="absolute top-0 right-0 m-1 cursor-pointer rounded-full bg-black/30 p-1"
-                    >
-                      <XIcon className="h-full w-full text-white" />
-                    </div>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-        )}
-        <Button
-          onClick={() => {
-            fileInputRef.current?.click();
-          }}
-          disabled={isCreatePostPending}
-          variant={"outline"}
-          className="cursor-pointer"
-        >
-          <ImageIcon />
-          이미지 추가
-        </Button>
-        <Button
-          disabled={isCreatePostPending}
-          onClick={handleCreatePostClick}
-          className="cursor-pointer"
-        >
-          저장
-        </Button>
-      </DialogContent>
-    </Dialog>
-  );
 }
